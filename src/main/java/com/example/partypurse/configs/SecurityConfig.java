352aa851a8d54fc47a763b5d2325a8beafb6fcf1
adziri <liubov.nikita@gmail.com>
2024-02-23 18:14:04 +0100

    added test logout

--- src/main/java/com/example/partypurse/configs/SecurityConfig.java
+++ src/main/java/com/example/partypurse/configs/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/user/**").authenticated()
                         .anyRequest().authenticated()
                 )
-
+                .logout(logout -> logout.logoutUrl("/api/v1/auth/singin"))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
