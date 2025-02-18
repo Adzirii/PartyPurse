ee4241c6f9fc07958260467b830785c9dac8886c
adziri <liubov.nikita@gmail.com>
2025-02-18 12:24:32 +0100

    added hello comment

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,3 @@
-
-#
-#COPY src src
-#COPY pom.xml pom.xml
-#
-#RUN mvn clean -Dmaven.test.skip package
-#
-#FROM bellsoft/liberica-openjdk-debian:latest
-#
-#RUN adduser --system spring-boot && addgroup --system spring-boot && adduser spring-boot spring-boot
-#USER spring-boot
-#
-#WORKDIR /app
-#
-#COPY --from=build target/PartyPurse-0.0.1-SNAPSHOT.jar ./partypurse.jar
-#
-#ENTRYPOINT ["java", "-jar", "./partypurse.jar"]
-
 FROM eclipse-temurin:21-alpine as base
 
 
@@ -38,21 +20,3 @@ FROM eclipse-temurin:21-alpine
 EXPOSE 8080
 COPY --from=builder /app/target/PartyPurse-0.0.1-SNAPSHOT.jar /PartyPurse.jar
 CMD ["java", "-jar", "/PartyPurse.jar"]
-
-
-
-
-
-#FROM maven:3.9.6-eclipse-temurin-21-jammy as builder
-#
-#WORKDIR /app
-#COPY . /app/.
-#RUN mvn -f /app/pom.xml clean package -Dmaven.test.skip=true
-#
-#
-#FROM eclipse-temurin:21-jre-alpine
-#WORKDIR /app
-#COPY --from=builder /app/target/*.jar /app/*.jar
-#
-#EXPOSE 8181
-#ENTRYPOINT ["java", "-jar", "/app/*.jar"]
---
+++ src/test/java/com/example/partypurse/controllers/UserControllerTest.java
@@ -0,0 +1,48 @@
+package com.example.partypurse.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTest {
+
+    @Mock
+    List<String> mockedList;
+    @Spy
+    List<String> spiedList = new ArrayList<String>();
+    @Test
+    public void whenNotUseMockAnnotation_thenCorrect() {
+
+        mockedList.add("one");
+        Mockito.verify(mockedList).add("one");
+        assertEquals(0, mockedList.size());
+
+        Mockito.when(mockedList.size()).thenReturn(100);
+        assertEquals(100, mockedList.size());
+    }
+
+
+    @Test
+    public void whenUseSpyAnnotation_thenSpyIsInjectedCorrectly() {
+        spiedList.add("one");
+        spiedList.add("two");
+
+        Mockito.verify(spiedList).add("one");
+        Mockito.verify(spiedList).add("two");
+
+        assertEquals(2, spiedList.size());
+
+        Mockito.doReturn(100).when(spiedList).size();
+        assertEquals(100, spiedList.size());
+    }
+
+}
