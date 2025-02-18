a79768b59ccc24a4447459dd7b7778fcc1bb34fd
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2025-02-18 12:37:52 +0100

    [Maksim] Making A Conflict

--- src/main/java/com/example/partypurse/models/User.java
+++ src/main/java/com/example/partypurse/models/User.java
@@ -19,7 +19,7 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long isid;
 
     @Column(unique = true)
     private String username;
